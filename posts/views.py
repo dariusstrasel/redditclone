5a75c03fef8a181379d25ce62770f37233b5c5b7
Darius Strasel <strasel.darius@gmail.com>
2017-04-08 19:22:24 -0400

    Refactored model import to be more explicit; Added validation check fo adding URL field to post model.

--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from . import models
+from .models import Post
 
 
 @login_required
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['url']:
-            post = models.Post()
+            post = Post()
             post.title = request.POST['title']
             post.url = request.POST['url']
+            if post.url.startswith('http://') or post.url.startswith('https://'):
+                post.url = request.POST['url']
+            else:
+                post.url = 'http://' + request.POST['url']
             post.published_date = timezone.datetime.now()
             post.author = request.user
             post.save()
-            return render(request, 'posts/home.html')
+            return redirect('posts/home.html')
         else:
             return render(request, 'posts/create.html', {'error': 'Title or URL is missing. '})
     else:
@@ -22,4 +26,21 @@ def create(request):
 
 
 def home(request):
-    return render(request, 'posts/home.html')
+    posts = Post.objects.order_by('votes_total')
+    return render(request, 'posts/home.html', {'posts': posts})
+
+
+def upvote(request, pk):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=pk)
+        post.votes_total += 1
+        post.save()
+        return redirect('home')
+
+
+def downvote(request, pk):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=pk)
+        post.votes_total -= 1
+        post.save()
+        return redirect('home')
